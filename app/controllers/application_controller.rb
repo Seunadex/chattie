35b3a555ed7640609c4ea9741a6f28bd1bea2419
seunadex <adekunleseun001@gmail.com>
2018-03-05 13:46:24 +0100

    fix issues with get_other_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :get_all_users
   skip_before_action :verify_authenticity_token
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -16,5 +17,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  def get_all_users
+    @users = User.get_users
   end
 end
