b33b08e06b739e66d0cf4c2b97d64b0d936908da
seunadex <adekunleseun001@gmail.com>
2018-11-04 14:55:33 +0100

    add acceptance tests with capybara and cucumber

---
+++ spec/features/session_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature "Session", type: :feature do
+  before(:all) do
+    @user = FactoryBot.create(:user, username: 'janedoe')
+  end
+  scenario 'allows a user to log in' do
+    visit root_path
+    click_on 'Log in'
+    fill_in 'Username or email', with: @user.username
+    fill_in 'Password', with: @user.password
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'does not allow user to log in with invalid credentials' do
+    visit root_path
+    click_on 'Log in'
+    fill_in 'Username or email', with: @user.username
+    fill_in 'Password', with: '478279'
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Username or email or password.'
+  end
+end
---
+++ spec/features/set_topic_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+require "spec_helper"
+
+describe "Set topic", :type => :feature do
+  before(:all) do
+    user = FactoryBot.create(:user, username: 'johndoe')
+    login_as(user, :scope => :user)
+  end
+
+  after(:all) do
+    Warden.test_reset!
+  end
+
+  scenario "allows an authenticated user to set topic", js: true do
+    visit chatrooms_path
+    expect(page).to have_content 'Chatrooms'
+    click_on 'New Chatroom'
+    expect(page).to have_content 'New Chatroom'
+    fill_in('Name', with: 'Lagos-alll')
+    click_on 'Create Chatroom'
+    expect(page).to have_content 'Chatroom was successfully created.'
+    expect(page).to have_content 'Lagos-all'
+    expect(page).to have_content 'Direct Messages'
+    click_on 'Add a topic'
+    expect(page).to have_content('New Topic')
+    fill_in("Topic", with: 'Some ridiculous chatroom')
+    click_on 'Set topic'
+    expect(page).to have_content('Topic has been set.')
+    expect(page).to have_content('Some ridiculous chatroom')
+  end
+end
