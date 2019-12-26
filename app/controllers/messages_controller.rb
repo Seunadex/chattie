acdf7482c7201015b41e744f21ea2ef478563be7
seunadex <adekunleseun001@gmail.com>
2019-12-26 14:44:16 +0100

    set up test coverage

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -41,18 +41,4 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:body)
   end
-
-  def emojify(content)
-    if content.present?
-      content.html_safe.to_str.gsub(/:([\w+-]+):/) do |match|
-        emoji = Emoji.find_by_alias($1)
-        if emoji
-          %(<img alt="#$1" src="#{image_path("emoji/#{emoji.image_filename}")}"
-          style="vertical-align:middle" width="20" height="20" />)
-        else
-          match
-        end
-      end.html_safe
-    end
-  end
 end
